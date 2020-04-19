89481f5037900cfdd1fe98d25424ed34fd3aefac
birdx0810 <birdx0810@gmail.com>
2020-04-20 00:27:24 +0800

    Finish HMM Matrices for Viterbi

--- hidden_markov_model.py
+++ hidden_markov_model.py
@@ -19,20 +19,18 @@ class HiddenMarkovModel(MarkovBase):
                 self.counts[char_first][char_target] = 0
 
         # construct {{current: next}: count} state count dictionary
+        self.init_state_prob = {"h": 0, "l": 0}
         self.state_high = {}
         self.state_low = {}
+        self.state_prob = {}
         self.state_count = {}
         self.state_change = {
-            "h2h": 0,
-            "h2l": 0,
-            "l2l": 0,
-            "l2h": 0,
+            "h": {"h": 0, "l": 0},
+            "l": {"h": 0, "l": 0},
         }
         self.state_change_prob = {
-            "h2h": 0,
-            "h2l": 0,
-            "l2l": 0,
-            "l2h": 0,
+            "h": {"h": 0, "l": 0},
+            "l": {"h": 0, "l": 0},
         }
 
         # decide the prob of deciding the first char of sequences: equal distribution
@@ -66,10 +64,50 @@ class HiddenMarkovModel(MarkovBase):
             self.state_high[first] = {}
             self.state_low[first] = {}
             for second, prob in v.items():
-                if prob >= 0.25:
+                if prob > 0.25:
                     self.state_high[first][second] = prob
-                elif prob < 0.25:
+                elif prob <= 0.25:
                     self.state_low[first][second] = prob
+
+        for k, v in self.state_high.items():
+            for _, prob in v.items():
+                self.init_state_prob["h"] += prob
+
+        for k, v in self.state_low.items():
+            for _, prob in v.items():
+                self.init_state_prob["l"] += prob
+
+        self.init_state_prob["h"] = self.init_state_prob["h"]/4
+        self.init_state_prob["l"] = self.init_state_prob["l"]/4
+
+        total_prob = 0
+        state_high_prob = {"a": 0, "c": 0, "g": 0, "t": 0}
+        state_low_prob = {"a": 0, "c": 0, "g": 0, "t": 0}
+
+        for key in state_high_prob.keys():
+            for k, v in self.state_high.items():
+                for element, prob in v.items():
+                    if key == element:
+                        state_high_prob[key] += prob
+                        total_prob += prob
+        
+        for key in state_high_prob.keys():
+            state_high_prob[key] = state_high_prob[key]/total_prob
+
+        total_prob = 0
+
+        for key in state_low_prob.keys():
+            for k, v in self.state_low.items():
+                for element, prob in v.items():
+                    if key == element:
+                        state_low_prob[key] += prob
+                        total_prob += prob
+        
+        for key in state_low_prob.keys():
+            state_low_prob[key] = state_low_prob[key]/total_prob
+
+        self.state_prob["h"] = state_high_prob
+        self.state_prob["l"] = state_low_prob
         return
 
     def get_state_change(self, seq):
@@ -95,22 +133,20 @@ class HiddenMarkovModel(MarkovBase):
                 elif p == s and p in self.state_count.keys():
                     self.state_count[p] += 1
         
-        for t, c in self.state_count.items():
-            if c > 0:
-                if t[1] in self.state_high[t[0]].keys() and t[2] in self.state_high[t[1]].keys():
-                    self.state_change["h2h"] += c
-                elif t[1] in self.state_high[t[0]].keys() and t[2] in self.state_low[t[1]].keys():
-                    self.state_change["h2l"] += c
-                elif t[1] in self.state_low[t[0]].keys() and t[2] in self.state_high[t[1]].keys():
-                    self.state_change["l2h"] += c
-                elif t[1] in self.state_low[t[0]].keys() and t[2] in self.state_low[t[1]].keys():
-                    self.state_change["l2l"] += c
-                else:
-                    print("Something is damn wrong")
-        
-        for t, c in self.state_change.items():
-            print(f"{t}: {c}")
-            self.state_change_prob[t] = c/sum(self.state_change.values())
+        for trigram, count in self.state_count.items():
+            if count > 0:
+                if trigram[1] in self.state_high[trigram[0]].keys() and trigram[2] in self.state_high[trigram[1]].keys():
+                    self.state_change["h"]["h"] += count
+                elif trigram[1] in self.state_high[trigram[0]].keys() and trigram[2] in self.state_low[trigram[1]].keys():
+                    self.state_change["h"]["l"] += count
+                elif trigram[1] in self.state_low[trigram[0]].keys() and trigram[2] in self.state_high[trigram[1]].keys():
+                    self.state_change["l"]["h"] += count
+                elif trigram[1] in self.state_low[trigram[0]].keys() and trigram[2] in self.state_low[trigram[1]].keys():
+                    self.state_change["l"]["l"] += count
+
+        for s_from, v in self.state_change.items():
+            for s_to, count in v.items():
+                self.state_change_prob[s_from][s_to] += count/(len(seq)-2)
         return
 
     def fit(self, seq):
@@ -130,15 +166,17 @@ class HiddenMarkovModel(MarkovBase):
             if index < pair_num:
                 self.counts[char][seq[index+1]] += 1
         
-        logging.info(f'Counts: {self.counts}')
+        # logging.info(f'Counts: {self.counts}')
         self._to_cond_prob()
-        logging.info(f'Cond_prob: {self.cond_prob}')
+        # logging.info(f'Cond_prob: {self.cond_prob}')
         self.get_state_prob()
-        logging.info(f'State_High: {self.state_high}')
-        logging.info(f'State_Low: {self.state_low}')
+        # logging.info(f'State_High: {self.state_high}')
+        # logging.info(f'State_Low: {self.state_low}')
+        logging.info(f'State_Prob: {self.state_prob}')
+        logging.info(f'Init_State_Prob: {self.init_state_prob}')
         self.get_state_change(seq)
-        logging.info(f'State_Count: {self.state_count}')
-        logging.info(f'State_Change: {self.state_change}')
+        # logging.info(f'State_Count: {self.state_count}')
+        # logging.info(f'State_Change: {self.state_change}')
         logging.info(f'State_Change_Prob: {self.state_change_prob}')
         return
 
