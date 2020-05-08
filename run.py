25efb7e9215758643939eea9bf79813b5560fe30
Rispolyv0n <navy.horizon.314@gmail.com>
2020-05-08 14:42:27 +0800

    Add timers

--- run.py
+++ run.py
@@ -1,14 +1,30 @@
 import logging
 from argparse import ArgumentParser
-import sys
+import timeit
 
 from markov_model import MarkovOrderZero, MarkovOrderOne, MarkovOrderTwo
 from hidden_markov_model import HiddenMarkovModel
+
+class RecordTime(object):
+    def __init__(self):
+        super().__init__()
+    
+    def start(self):
+        self.start_time = timeit.default_timer()
+
+    def stop(self):
+        self.end_time = timeit.default_timer()
+
+    def get_duration(self):
+        return self.end_time - self.start_time
+
 
 def main():
     parser = ArgumentParser()
     parser.add_argument('-p', '--print-detail', help='Whether to print details.', action='store_true')
     args = parser.parse_args()
+
+    timer = RecordTime()
 
     if args.print_detail:
         logging.basicConfig(level=logging.INFO,
@@ -48,17 +64,43 @@ def main():
     for model_name in model_infos.keys():
         print(f'\n=== {model_name} ===')
         model = model_infos[model_name]['class'](vocab=set(s), random_seed=17)
+        timer.start()
         model.fit(s)
-        print(f'Target sequence generation probability: {model.generating_prob(s)}')
-        print(f'Another 100k sequence generation probability: {model.generating_prob(test_s)}')
+        timer.stop()
+        fit_time = timer.get_duration()
+        timer.start()
+        print(f'Target sequence generation probability:\t{model.generating_prob(s)}')
+        timer.stop()
+        calculate_gp_time = timer.get_duration()
+        timer.start()
+        print(f'Another 100k sequence generation probability:\t{model.generating_prob(test_s)}')
+        timer.stop()
+        calculate_another_gp_time = timer.get_duration()
+        print(f'Time - fitting model:\t{fit_time:.3f} sec')
+        print(f'Time - calculating target sequence generation probability:\t{calculate_gp_time:.3f} sec')
+        print(f'Time - calculating another 100k sequence generation probability:\t{calculate_another_gp_time:.3f} sec')
 
     # run hidden Markov models
     print(f'\n=== Hidden Markov Model ===')
     hidden_markov_model = HiddenMarkovModel(vocab=set(s), random_seed=17)
+    timer.start()
     hidden_markov_model.fit(s)
-    print(f'Target sequence generation probability: {hidden_markov_model.generating_prob(s)}')
-    print(f'Another 100k sequence generation probability: {hidden_markov_model.generating_prob(test_s)}')
+    timer.stop()
+    fit_time = timer.get_duration()
+    timer.start()
+    print(f'Target sequence generation probability:\t{hidden_markov_model.generating_prob(s)}')
+    timer.stop()
+    calculate_gp_time = timer.get_duration()
+    timer.start()
+    print(f'Another 100k sequence generation probability:\t{hidden_markov_model.generating_prob(test_s)}')
+    timer.stop()
+    calculate_another_gp_time = timer.get_duration()
+    print(f'Time - fitting model:\t{fit_time:.3f} sec')
+    print(f'Time - calculating target sequence generation probability:\t{calculate_gp_time:.3f} sec')
+    print(f'Time - calculating another 100k sequence generation probability:\t{calculate_another_gp_time:.3f} sec')
+
     # write state sequences to text files
+    logging.info('Writing state sequences to file...')
     with open('./state_seq_s.txt', 'w') as f:
         f.write(''.join(hidden_markov_model.state_sequence(s)))
     with open('./state_seq_test_s.txt', 'w') as f:
